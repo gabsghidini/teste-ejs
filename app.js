30efd7211697683628303ef6df88079e8da6cec0
Gabs Ghidini <gabsghidini@gmail.com>
2024-11-12 21:22:35 -0300

    FEAT: vercel.json

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}! 😎`);
 });
