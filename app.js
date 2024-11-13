55f43d15ddbe4b311fdfcf8b59ac274eef2c9c6c
Gabs Ghidini <gabsghidini@gmail.com>
2024-11-12 21:13:15 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}! 😎`);
+});
